4accca948fba97a17b0154bfce99d472c480e15b
rwetherall <rwetherall@alfresco.com>
2014-08-31 13:55:56 +1000

    CMIS improvements
    Accidently excluded from last commit

--- contentcraft/src/main/java/org/alfresco/contentcraft/cmis/CMIS.java
+++ contentcraft/src/main/java/org/alfresco/contentcraft/cmis/CMIS.java
@@ -1,11 +1,12 @@
-/**
- * 
- */
+
 package org.alfresco.contentcraft.cmis;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.alfresco.cmis.client.AlfrescoFolder;
 import org.apache.chemistry.opencmis.client.api.CmisObject;
 import org.apache.chemistry.opencmis.client.api.Folder;
 import org.apache.chemistry.opencmis.client.api.Session;
@@ -13,19 +14,55 @@ import org.apache.chemistry.opencmis.client.api.SessionFactory;
 import org.apache.chemistry.opencmis.client.runtime.SessionFactoryImpl;
 import org.apache.chemistry.opencmis.commons.SessionParameter;
 import org.apache.chemistry.opencmis.commons.enums.BindingType;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 /**
+ * CMIS helper implementation.
+ * 
  * @author Roy Wetherall
  */
 public class CMIS 
-{
-	private static final String ALFRSCO_ATOMPUB_URL = "http://localhost:8080/alfresco/service/cmis";
-	private static final String REPOSITORY_ID = "cc6265b4-ba96-4289-b5c4-5a9ab77f8999";
-	private static final String USER = "admin";
-	private static final String PASSWORD = "admin";
+{	
+	private static final String CMIS_CONFIG_FILE = "cmis.json";
+	private static boolean isInit = false;
+	
+	private static String ALFRSCO_ATOMPUB_URL; // = "http://localhost:8080/alfresco/service/cmis";
+	private static String REPOSITORY_ID; // = "cc6265b4-ba96-4289-b5c4-5a9ab77f8999";
+	private static String USER; // = "admin";
+	private static String PASSWORD; // = "admin";
+	
+	private static void init()
+	{
+		try
+		{
+			InputStream is = CMIS.class.getClassLoader().getResourceAsStream(CMIS_CONFIG_FILE);
+			InputStreamReader reader = new InputStreamReader(is);
+					
+			JSONParser jsonParser = new JSONParser();
+			JSONObject jsonCMIS = (JSONObject)jsonParser.parse(reader);
+			
+			CMIS.ALFRSCO_ATOMPUB_URL = (String)jsonCMIS.get("connection-url");
+			CMIS.REPOSITORY_ID = (String)jsonCMIS.get("repository-id");
+			CMIS.USER = (String)jsonCMIS.get("user");
+			CMIS.PASSWORD = (String)jsonCMIS.get("password");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
 	
 	public static Session connect() 
 	{
+		if (isInit == false)
+		{
+			System.out.println("Init connection details.");
+			CMIS.init();
+			isInit = true;
+		}
+		
 		SessionFactory sessionFactory = SessionFactoryImpl.newInstance();
 		Map<String, String> parameter = new HashMap<String, String>();
 		parameter.put(SessionParameter.USER, USER);
@@ -33,21 +70,34 @@ public class CMIS
 		parameter.put(SessionParameter.ATOMPUB_URL, ALFRSCO_ATOMPUB_URL);
 		parameter.put(SessionParameter.BINDING_TYPE, BindingType.ATOMPUB.value());
 		parameter.put(SessionParameter.REPOSITORY_ID, REPOSITORY_ID);
+		parameter.put(SessionParameter.OBJECT_FACTORY_CLASS, "org.alfresco.cmis.client.impl.AlfrescoObjectFactoryImpl");
 		
 		return sessionFactory.createSession(parameter);
 	}
 	
-	public static Folder getSiteRoot(Session session, String siteName)
+	public static String testConnect()
+	{
+		String result = "Unable to connect to " + ALFRSCO_ATOMPUB_URL;
+		Session session = CMIS.connect();		
+		Folder rootFolder = session.getRootFolder();
+		if (rootFolder != null)
+		{
+			result = "Successfully connected to " + ALFRSCO_ATOMPUB_URL; 
+		}
+		return result;
+	}
+	
+	public static AlfrescoFolder getSiteRoot(Session session, String siteName)
 	{
 		String docLibPath = "/sites/" + siteName + "/documentLibrary";
 		
 		CmisObject folder = session.getObjectByPath(docLibPath);
-		if (!(folder instanceof Folder))
+		if (!(folder instanceof AlfrescoFolder))
 		{
 			throw new RuntimeException("Unable to get site root for site '" + siteName + "'");
 		}
 		
-		return (Folder)folder;
+		return (AlfrescoFolder)folder;
 	}
 
 }
--- contentcraft/src/main/java/org/alfresco/contentcraft/command/cmis/CMISCommandExecuter.java
+++ contentcraft/src/main/java/org/alfresco/contentcraft/command/cmis/CMISCommandExecuter.java
@@ -6,8 +6,6 @@ import java.util.Map;
 import org.alfresco.contentcraft.cmis.CMIS;
 import org.alfresco.contentcraft.command.BaseCommandExecuter;
 import org.alfresco.contentcraft.command.CommandUsageException;
-import org.apache.chemistry.opencmis.client.api.Folder;
-import org.apache.chemistry.opencmis.client.api.Session;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
@@ -25,13 +23,7 @@ public class CMISCommandExecuter extends BaseCommandExecuter
 
 	public void onCommandImpl(CommandSender sender, Command command, String label, String[] args) throws CommandUsageException
 	{
-		// get the command to execute
-
-		Session session = CMIS.connect();		
-		Folder rootFolder = session.getRootFolder();
-		
-		System.out.println("The root folder is " + rootFolder.getName());
-	}
-	
-
+		String result = CMIS.testConnect();
+		sender.sendMessage(result);
+	}	
 }
